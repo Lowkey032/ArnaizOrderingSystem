0483498e89a2f28beaab632306e5f2b8a5848b9a
Lowkey032 <dreadgoat032@gmail.com>
2025-02-27 18:46:58 +0800

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,37 @@
+let navbar = () => {
+  return `<div id="upr">
+  <div id="logo">
+    <img src="./images/navbar_images/logo.PNG" alt="" />
+  </div>
+  <div id="social">
+    <img src="./images/navbar_images/social_1.jpg" alt="" />
+    <a href="https://www.facebook.com/KindMeal.my"
+      ><img src="./images/navbar_images/social_2.jpg" alt=""
+    /></a>
+    <a href="https://twitter.com/KindMeal"
+      ><img src="./images/navbar_images/social_3.jpg" alt=""
+    /></a>
+  </div>
+  <div id="loginAndsignup">
+    <h1><a href="login.html">login</a></h1>
+    <button>Facebook</button>
+    <button>Email</button>
+    <h1><a href="signup.html">Signup</a></h1>
+  </div>
+</div>
+
+<div id="lower">
+  <div><a href="index.html">Home</a></div>
+  <div><a href="mealdeals.html">Meal Deals</a></div>
+  <div><a href="kindmoment.html">KindMoments</a></div>
+  <div><a href="hotpicks.html">Hot Picks</a></div>
+  <div><a href="recipes.html">Recipes</a></div>
+  <div><a href="directory.html">Directory</a></div>
+  <div><a href="artical.html">Articles</a></div>
+  <div><a href="help.html">Help</a></div>
+  <div><a href="cart.html">Cart</a></div>
+</div> 
+   `;
+};
+
+export default navbar;
